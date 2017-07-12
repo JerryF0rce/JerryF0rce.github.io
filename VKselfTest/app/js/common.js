4325f1f07a85a689f5d7764141936a423e951acb
Jerry Force <john1kote@gmail.com>
2017-07-12 23:49:37 +0300

    21
    21

--- VKselfTest/app/js/common.js
+++ VKselfTest/app/js/common.js
@@ -1,5 +1,20 @@
-$(function() {
+$(document).ready(function() {
 
-	// Custom JS
+   $("#my-menu").mmenu({
+   	extensions: [ 'pagedim-black' ],
+   	navbar: {
+   	title: ' '
+   	}
+   });
+
+   var API = $('#my-menu').data('mmenu');
+
+		API.bind('open:start', function(){
+			$('.hamburger').addClass('is-active');
+		});
+		API.bind('close:before', function(){
+			$('.hamburger').removeClass('is-active');
+	});
 
 });
+
