edeb18e0f44489e6ef8af394399e00d5af22344a
Jerry Force <john1kote@gmail.com>
2017-07-15 01:15:09 +0300

    testQ

--- Portland/app/js/common.js
+++ Portland/app/js/common.js
@@ -3,16 +3,16 @@ $(document).ready(function() {
    $("#my-menu").mmenu({
    	extensions: [ 'pagedim-black' ],
    	navbar: {
-   	title: ' '
+   	title: '<div class="logo m-logo"><a href="#">Portland</a></div>'
    	}
    });
 
    var API = $('#my-menu').data('mmenu');
 
-		API.bind('open:start', function(){
+		API.bind('open:finish', function(){
 			$('.hamburger').addClass('is-active');
 		});
-		API.bind('close:before', function(){
+		API.bind('close:finish', function(){
 			$('.hamburger').removeClass('is-active');
 	});
 
